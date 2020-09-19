8ddab8ad63f433c639ceedf04d982453cc293557
taufique74 <taufiquzzaman.peyash@northsouth.edu>
2020-09-20 03:27:04 +0600

    modified profile information

--- config/index.js
+++ config/index.js
@@ -1,15 +1,15 @@
 module.exports = {
   pathPrefix: "/", // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
-  siteTitle: "Thomas Maximini", // Navigation and Site Title
-  siteTitleAlt: "Thomas Maximini - full stack web developer", // Alternative Site title for SEO
-  siteTitleShort: "ThomasMaximini", // short_name for manifest
-  siteHeadline: "Full stack web developer from Germany", // Headline for schema.org JSONLD
-  siteUrl: "https://thomasmaximini.com", // Domain of your site. No trailing slash!
+  siteTitle: "Taufiquzzaman Peyash", // Navigation and Site Title
+  siteTitleAlt: "Taufiquzzaman Peyash - full stack web developer", // Alternative Site title for SEO
+  siteTitleShort: "TaufiquePeyash", // short_name for manifest
+  siteHeadline: "Full stack web developer from Dhaka", // Headline for schema.org JSONLD
+  siteUrl: "https://peyash.me", // Domain of your site. No trailing slash!
   siteLanguage: "en", // Language Tag on <html> element
   siteLogo: "/logo.png", // Used for SEO and manifest
   siteDescription:
-    "Hi! I am Thomas, a freelance software engineer and consultant based in Germany. I'm an expert at modern web technologies such as Javascript, React, Node.js, AWS, serverless. Get in touch if you want to work with me!",
+    "Hi! I am Peyash, a freelance software engineer and consultant based in Germany. I'm an expert at modern web technologies such as Javascript, React, Node.js, AWS, serverless. Get in touch if you want to work with me!",
   author: "ThomasMaximini <tmaximini@gmail.com>", // Author for schema.org JSONLD
 
   keywords:
